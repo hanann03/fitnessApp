b95bc9bfc4abed91827d212cc6b7388039414bbe
Hanan <nny2003@hotmail.com>
2024-07-28 12:31:02 +0300

    initial design

---
+++ app/src/main/java/com/example/fitnessapp2/Home.java
@@ -0,0 +1,24 @@
+package com.example.fitnessapp2;
+
+public class Home {
+
+        public String id;
+        public int image;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
